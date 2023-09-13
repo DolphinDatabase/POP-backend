e85f22ce58ae168792de48ec15450dbbfe948d1f
Pedro Motta <pedromotta321r@gmail.com>
2023-09-13 19:30:06 -0300

    POP-8 add user table

--- ddl/schema.sql
+++ ddl/schema.sql
@@ -1,3 +1,11 @@
+CREATE TABLE usr_usuario (
+    usr_id SERIAL PRIMARY KEY,
+    usr_nome VARCHAR(150),
+    usr_email VARCHAR(255),
+    usr_senha VARCHAR(255),
+    usr_permissao BOOLEAN
+)
+
 CREATE TABLE ccl_ciclo (
     ccl_id INT PRIMARY KEY,
     ccl_descricao VARCHAR(150)

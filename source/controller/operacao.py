4d914b2a41873d657dc9a6782ad57d4af018f22b
marcoskisto <marcosoiseau@gmail.com>
2023-09-18 20:01:03 -0300

    POP 15 - Correcao no modulo operacao_controller

--- source/controller/operacao.py
+++ source/controller/operacao.py
@@ -10,13 +10,13 @@ Base.metadata.create_all(bind=engine)
 
 router = APIRouter(prefix="/operacao")
 
-@router.get("/", response_model=list[schemas.User])
+@router.get("/", response_model=list[schemas.Operacao])
 async def read_operacaos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     operacaos = await crud.get_operacaos(db, skip=skip, limit=limit)
     return operacaos
 
 
-@router.get("/{id}", response_model=schemas.User)
+@router.get("/{id}", response_model=schemas.Operacao)
 async def read_operacao(id: int, db: Session = Depends(get_db)):
     result = await crud.get_operacao(db, id=id)
     if result is None:

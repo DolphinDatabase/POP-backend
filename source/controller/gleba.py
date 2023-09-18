2e4c5547b625abe394997b3964834b9019748cca
marcoskisto <marcosoiseau@gmail.com>
2023-09-18 20:45:53 -0300

    POP 15 - Correção de importacao de módulos

--- source/controller/gleba.py
+++ source/controller/gleba.py
@@ -1,18 +1,18 @@
 from fastapi import Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
-import crud
+import crud.gleba as crud_gleba
 from schema import schemas
 from database import get_db
 
 router = APIRouter(prefix="/gleba")
 
 @router.get("/", response_model=list[schemas.Gleba])
-async def read_operacaos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return await crud.get_operacaos(db, skip=skip, limit=limit)
+def read_glebas(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud_gleba.get_glebas(db, skip=skip, limit=limit)
 
 @router.get("/{id}", response_model=schemas.Gleba)
-async def read_operacao(id: int, db: Session = Depends(get_db)):
-    result = await crud.get_operacao(db, id=id)
+def read_glebas(id: int, db: Session = Depends(get_db)):
+    result = crud_gleba.get_gleba(db, id=id)
     if result is None:
         raise HTTPException(status_code=404, detail="Not found")
     return result
--- source/controller/operacao.py
+++ source/controller/operacao.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-import crud
+from crud import operacao as crud_operacao
 from schema import schemas
 from database import engine, Base, get_db
 
@@ -11,14 +11,14 @@ Base.metadata.create_all(bind=engine)
 router = APIRouter(prefix="/operacao")
 
 @router.get("/", response_model=list[schemas.Operacao])
-async def read_operacaos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    operacaos = await crud.get_operacaos(db, skip=skip, limit=limit)
+def read_operacaos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    operacaos = crud_operacao.get_operacaos(db, skip=skip, limit=limit)
     return operacaos
 
 
 @router.get("/{id}", response_model=schemas.Operacao)
-async def read_operacao(id: int, db: Session = Depends(get_db)):
-    result = await crud.get_operacao(db, id=id)
+def read_operacao(id: int, db: Session = Depends(get_db)):
+    result = crud_operacao.get_operacao(db, id=id)
     if result is None:
         raise HTTPException(status_code=404, detail="Not found")
     return result

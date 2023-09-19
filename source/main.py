79f9341b828513aa83284e2aa80b5f45c1ad306c
Pedro Motta <pedromotta321r@gmail.com>
2023-09-18 21:23:23 -0300

    POP-5 add cors

--- source/main.py
+++ source/main.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI
 import controller
 import uvicorn
 from database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(controller.termo_router)
 app.include_router(controller.auth_router)

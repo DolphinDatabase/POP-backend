160e3a73f1c43a50b7b544dee167c7e900aeac2c
marcoskisto <marcosoiseau@gmail.com>
2023-09-18 21:13:03 -0300

    POP 15 - Adequacao de Gleba e Operacoa ao padrao de codigo do time

--- source/controller/gleba.py
+++ source/controller/gleba.py
@@ -1,18 +1,19 @@
 from fastapi import Depends, HTTPException, APIRouter
-from sqlalchemy.orm import Session
-import crud.gleba as crud_gleba
+from service import gleba_service
 from schema import schemas
 from database import get_db
 
 router = APIRouter(prefix="/gleba")
 
-@router.get("/", response_model=list[schemas.Gleba])
-def read_glebas(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return crud_gleba.get_glebas(db, skip=skip, limit=limit)
 
-@router.get("/{id}", response_model=schemas.Gleba)
-def read_glebas(id: int, db: Session = Depends(get_db)):
-    result = crud_gleba.get_gleba(db, id=id)
-    if result is None:
-        raise HTTPException(status_code=404, detail="Not found")
-    return result
+@router.get('/', response_model=list[schemas.Gleba])
+async def get_glebas():
+    return gleba_service.get_glebas()
+
+
+@router.get('/{id}', response_model=schemas.Gleba)
+async def get(id:int):
+    try:
+        return gleba_service.get_gleba(id)
+    except:
+        raise HTTPException(404)
--- source/controller/operacao.py
+++ source/controller/operacao.py
@@ -1,24 +1,23 @@
 
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, HTTPException
 
-from crud import operacao as crud_operacao
+from service import operacao_service
 from schema import schemas
-from database import engine, Base, get_db
+from database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 
 router = APIRouter(prefix="/operacao")
 
-@router.get("/", response_model=list[schemas.Operacao])
-def read_operacaos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    operacaos = crud_operacao.get_operacaos(db, skip=skip, limit=limit)
-    return operacaos
+
+@router.get('/', response_model=list[schemas.Operacao])
+async def get_operacaos():
+    return operacao_service.get_operacaos()
 
 
-@router.get("/{id}", response_model=schemas.Operacao)
-def read_operacao(id: int, db: Session = Depends(get_db)):
-    result = crud_operacao.get_operacao(db, id=id)
-    if result is None:
-        raise HTTPException(status_code=404, detail="Not found")
-    return result
+@router.get('/{id}', response_model=schemas.Gleba)
+async def get(id:int):
+    try:
+        return operacao_service.get_operacao(id)
+    except:
+        raise HTTPException(404)

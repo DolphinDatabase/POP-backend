837f52beae7dd294a7e9924118162ce7674538f4
marcoskisto <marcosoiseau@gmail.com>
2023-09-18 22:16:39 -0300

    POP 15 - Casting Polygon to Geojson

--- source/model/gleba.py
+++ source/model/gleba.py
@@ -1,7 +1,7 @@
 from geoalchemy2 import Geography
 from sqlalchemy import Column, ForeignKey, Integer
 from sqlalchemy.orm import relationship
-
+from geoalchemy2.functions import ST_AsGeoJSON
 from database import Base
 
 
@@ -9,7 +9,11 @@ class Gleba(Base):
     __tablename__ = "glb_gleba"
 
     id = Column("glb_id", Integer, primary_key=True, index=True)
-    poligono = Column("glb_poligono", Geography('POLYGON'), nullable=False)
+    _poligono = Column("glb_poligono", Geography('POLYGON'), nullable=False)
 
     operacao_id = Column("glb_opr", Integer, ForeignKey("opr_operacao.opr_id"))
     operacao = relationship("Operacao", back_populates="gleba")
+
+    @property
+    def poligono(self):
+        return ST_AsGeoJSON(self._poligono)

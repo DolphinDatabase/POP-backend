4b0298ea55cb0c38f1602b4eed1d0ee11fbec06d
Pedro Motta <pedromotta321r@gmail.com>
2023-09-22 19:16:06 -0300

    POP-15 rota gleba por estado

--- source/controller/gleba.py
+++ source/controller/gleba.py
@@ -7,11 +7,13 @@ from .auth_controller import get_current_user
 
 router = APIRouter(prefix="/gleba")
 
+@router.get('/estado/{estado}', response_model=schemas.FeatureCollections)
+async def get_by_estado(estado:str,user: Annotated[Usuario, Depends(get_current_user)],skip: int = 0, limit: int = 100):
+    return gleba_service.get_gleba_by_estado(estado,skip,limit)
 
 @router.get('/', response_model=schemas.FeatureCollections)
 async def get_glebas(user: Annotated[Usuario, Depends(get_current_user)],skip: int = 0, limit: int = 100):
     return gleba_service.get_glebas(skip,limit)
-
 
 @router.get('/{id}', response_model=schemas.Feature)
 async def get(id:int, user: Annotated[Usuario, Depends(get_current_user)]):
--- source/service/gleba_service.py
+++ source/service/gleba_service.py
@@ -1,5 +1,6 @@
 from database import SessionLocal
-from model.gleba import Gleba
+from model import Gleba,Operacao,Estado
+from sqlalchemy.orm import aliased
 
 def get_glebas(skip: int = 0, limit: int = 100):
     with SessionLocal() as db:
@@ -25,3 +26,16 @@ def get_gleba(id: int):
             'operacao_id':gleba.operacao_id     
         }
     return feature
+
+def get_gleba_by_estado(sigla: str,skip:int=0,limit:int=100):
+    with SessionLocal() as db:
+        glebas = db.query(Gleba).join(Operacao).join(Estado).filter(Estado.descricao == sigla).offset(skip).limit(limit).all()
+        features = []
+        for glb in glebas:
+            feature = glb.poligono
+            feature['properties'] = {
+                'gleba_id':glb.id,
+                'operacao_id':glb.operacao_id     
+            }
+            features.append(feature)
+    return {'type':'FeatureCollection','features':features}

866189a72df5b20bc421570d336d633b18dec0e8
Pedro Motta <pedromotta321r@gmail.com>
2023-11-02 19:36:06 -0300

    fix modeling

--- source/configuration.py
+++ source/configuration.py
@@ -5,7 +5,7 @@ OAUTH_SECRET_KEY = os.getenv("OAUTH_SECRET_KEY", "SECRET")
 OAUTH_ALGORITHM = os.getenv("OAUTH_ALGORITHM", "HS256")
 OAUTH_ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("OAUTH_TOKEN_EXPIRE", "30"))
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://pop:pop@localhost:5432/pop")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:root@localhost:5432/postgres")
 # DATABASE_URL = os.getenv("DATABASE_URL",
 #                          "postgresql://postgres:admin@localhost:5432/postgres")
 LOCAL_REDIS_URL = os.environ.get("REDIS_URL", "redis://127.0.0.1:6379")

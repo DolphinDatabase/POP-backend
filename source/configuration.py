7b280a696aa2646b3aaab0bf9acb8842caccf78e
Victor Cavichioli <victor.cavichioli@ericsson.com>
2023-11-09 19:21:05 -0300

    Creating Master

--- source/configuration.py
+++ source/configuration.py
@@ -6,8 +6,10 @@ OAUTH_ALGORITHM = os.getenv("OAUTH_ALGORITHM", "HS256")
 OAUTH_ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("OAUTH_TOKEN_EXPIRE", "30"))
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:root@localhost:5432/postgres")
+
 # DATABASE_URL = os.getenv("DATABASE_URL",
 #                          "postgresql://postgres:admin@localhost:5432/postgres")
+
 LOCAL_REDIS_URL = os.environ.get("REDIS_URL", "redis://127.0.0.1:6379")
 LOCAL_CASSANDRA_URL = os.environ.get("CASSANDRA_URL", "localhost")
 
